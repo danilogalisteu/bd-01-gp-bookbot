725f1512f69f37ac43953c3e170c0dcf1d3f50e1
danilogalisteu <danilogalisteu@gmail.com>
2024-02-04 20:34:26 -0300

    printing report

--- main.py
+++ main.py
@@ -23,18 +23,21 @@ def get_letter_dict(text):
 
 def main():
     book_path = "books/frankenstein.txt"
+    print(f"--- Begin report of {book_path} ---")
 
     book_text = get_file_text(book_path)
-    print(f"{get_word_count(book_text)} words")
+    print(f"{get_word_count(book_text)} words found in the document\n")
 
     letter_dict = get_letter_dict(book_text)
 
     letter_list = [{'name':k, 'num':letter_dict[k]} for k in letter_dict]
-    print(letter_list)
 
     sort_on = lambda d: d['num']
     letter_list.sort(key=sort_on, reverse=True)
-    print(letter_list)
+
+    for letter in letter_list:
+        if letter['name'].isalpha():
+            print(f"The '{letter['name']}' character was found {letter['num']} times")
 
 
 main()
