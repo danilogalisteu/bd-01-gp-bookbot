5daf4a0e64c5ac6d9dacfcbbc4bcb16626ff167b
danilogalisteu <danilogalisteu@gmail.com>
2024-02-04 20:31:08 -0300

    added function for letter dict

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ def get_word_count(text):
     return len(text.split())
 
 
+def get_letter_dict(text):
+    letter_dict = {}
+    for letter in text.lower():
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
+
+
 def main():
     book_path = "books/frankenstein.txt"
+
     book_text = get_file_text(book_path)
     print(f"{get_word_count(book_text)} words")
+
+    letter_dict = get_letter_dict(book_text)
+    print(letter_dict)
 
 
 main()
