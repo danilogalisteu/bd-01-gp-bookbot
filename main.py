50ddee8c33ffe8c860ec71c504fa08b793bedbb5
Danilo Galisteu <danilogalisteu@gmail.com>
2025-03-18 14:46:29 -0300

    added CLI argument for book path

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_num_words, get_letter_dict, sort_letter_dict
 
 
@@ -9,7 +11,12 @@ def get_book_text(path):
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
 
